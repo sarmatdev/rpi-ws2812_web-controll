3d6a98424822adaf62156a07a639f4db50b1b78a
Sarmat Khachirov <skhachirov12@gmail.com>
2020-02-12 19:04:39 +0300

    Import effects and test them

--- src/app.js
+++ src/app.js
@@ -1,36 +1,21 @@
-const ws281x = require('rpi-ws281x-native');
+// const ws281x = require('rpi-ws281x-native');
 const express = require('express');
 const path = require('path');
 
+// Effects
+const plain = require('./effects/plain');
+const rainbow = require('./effects/rainbow');
+const iterate = require('./effects/iterate');
+const sandbox = require('./effects/sandbox')
+
 const app = express();
 
-app.use(express.static(path.join(__dirname, '../public')));
+app.use(express.static(__dirname + '/public'));
 
-app.get('/rainbow', (req, res) => {
-  res.sendFile(path.join(__dirname, '../public/rainbow.html'));
-  const NUM_LEDS = 8
-    const pixelData = new Uint32Array(NUM_LEDS);
+// sandbox();
 
-  ws281x.init(NUM_LEDS);
+rainbow();
 
-  // ---- trap the SIGINT and reset before exit
-  process.on('SIGINT', function() {
-    ws281x.reset();
-    process.nextTick(function() {
-      process.exit(0);
-    });
-  });
-
-  for (var i = 0; i < NUM_LEDS; i++) {
-    pixelData[i] = 0x0051ff;
-  }
-  ws281x.render(pixelData);
-  console.log(pixelData)
-
-  // ---- animation-loop
-  ws281x.setBrightness(10);
-
-  console.log('Press <ctrl>+C to exit.');
-});
+// iterate();
 
 app.listen(8000, () => console.log('Server Started!'));
