8291bd091081e0d11236d32cadfc845311cb5af1
Sarmat Khachirov <skhachirov12@gmail.com>
2020-02-13 09:18:48 +0300

    Create controller component

---
+++ client/src/components/Controller.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <button @click="effect('plain')">Plain Color</button>
+    <button @click="effect('rainbow')">Rainbow</button>
+    <button @click="effect('iterate')">Iterate</button>
+    <button @click="effect('sandbox')">Sanbox</button>
+  </div>
+</template>
+
+<script>
+import io from 'socket.io-client';
+
+export default {
+  data() {
+    return {
+      socket: {}
+    };
+  },
+  created() {
+    this.socket = io('http://192.168.1.107:8000');
+  },
+  methods: {
+    effect(type) {
+      this.socket.emit('effect', type);
+    }
+  }
+};
+</script>
+
+<style></style>
