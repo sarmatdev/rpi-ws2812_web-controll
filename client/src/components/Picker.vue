000d31a94481e518dc5aeb933d73558e96f02810
Sarmat Khachirov <skhachirov12@gmail.com>
2020-02-13 13:33:15 +0300

    Create color picker component

---
+++ client/src/components/Picker.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-row>
+    <v-col cols="12">
+      <v-row justify="center" aligin="center">
+        <v-color-picker v-model="color"></v-color-picker>
+      </v-row>
+      {{ showColor() }}
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      color: {}
+    };
+  },
+  methods: {
+    showColor() {
+      console.log(this.color.hex);
+    }
+  }
+};
+</script>
+
+<style></style>
